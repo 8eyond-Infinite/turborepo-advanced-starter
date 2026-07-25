19b65cdc0aa0b488e894bd3a05d0900002b40a84
Trần Hoàng Anh Tú <trhgatu.dev@gmail.com>
2026-07-26 01:04:41 +0700

    fix: build vercel

--- packages/contracts/tsup.config.ts
+++ packages/contracts/tsup.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'tsup';
+import ts from 'typescript';
+
+const ignoreDeprecations = Number.parseInt(ts.versionMajorMinor, 10) >= 6 ? '6.0' : '5.0';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: {
+    compilerOptions: {
+      ignoreDeprecations,
+    },
+  },
   clean: true,
   minify: false,
   sourcemap: true,
